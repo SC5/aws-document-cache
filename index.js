84295b4a580148857aaa65db00640a1e39351bab
Mikael Puittinen <mikael@iki.fi>
2015-07-22 10:35:07 +0300

    Fix old references to successCb and package description

--- index.js
+++ index.js
@@ -35,68 +35,70 @@ function getCacheKey(type, identifier) {
     return type + "-" + identifier;
 }
 
-function getDoc(type, identifier) {
-    return new Promise(function(successCb, errorCb) {
-
-        var cacheKey = getCacheKey(type, identifier);
-        var now = new Date().getTime();
-        // First check the memory cache
-        if (memcache[cacheKey] != null) {
-            var doc = memcache[cacheKey];
-            if ((now - doc.cacheMeta.memCacheTimeStamp) < memCacheLifetime ) {
-                // Cache is valid
-                doc.cacheMeta.src = "memcache";
-                return sucecssCb(doc);
-            }
+function getDoc(type, identifier, callback) {
+    if (callback == null) {
+        callback = function() {};
+    }
+    var cacheKey = getCacheKey(type, identifier);
+    var now = new Date().getTime();
+    // First check the memory cache
+    if (memcache[cacheKey] != null) {
+        var doc = memcache[cacheKey];
+        if ((now - doc.cacheMeta.memCacheTimeStamp) < memCacheLifetime ) {
+            // Cache is valid
+            doc.cacheMeta.src = "memcache";
+            return callback(null, doc);
         }
-        if (cacheTable == null) {
-            return successCb(null);
-        }
-        // Not in memory cache -- check now in DynamoDB
-        var params = {
-            Key: {'cacheKey' : cacheKey},
-            TableName: cacheTable
-        };
-        
-        dynamoDB.getItem(params, function(err, data) {
-            if (err) {
-                errorCb("Cache error " + err);
-            } else {
-                if (data.Item != null) {
-                    var doc = data.Item;
-                    var meta = doc.cacheMeta || {cacheTimeStamp : 0} ; // Fallback in case of schema changes
+    }
+    if (cacheTable == null) {
+        return callback(null,null);
+    }
+    
+    // Not in memory cache -- check now in DynamoDB
+    var params = {
+        Key: {'cacheKey' : cacheKey},
+        TableName: cacheTable
+    };
 
-                    if ((now - meta.cacheTimestamp) < dbCacheLifetime ) {
-                        // Cache is valid
-                        memcache[cacheKey] = doc;
-                        doc.cacheMeta.src = "dbcache";
-                        doc.cacheMeta.memCacheTimeStamp = new Date().getTime();
-                        return successCb(doc);
-                    }
+    dynamoDB.getItem(params, function(err, data) {
+        if (err) {
+            return callback("Cache error " + err, null);
+        } else {
+            if (data.Item != null) {
+                var doc = data.Item;
+                var meta = doc.cacheMeta || {cacheTimeStamp : 0} ; // Fallback in case of schema changes
+
+                if ((now - meta.cacheTimestamp) < dbCacheLifetime ) {
+                    // Cache is valid
+                    memcache[cacheKey] = doc;
+                    doc.cacheMeta.src = "dbcache";
+                    doc.cacheMeta.memCacheTimeStamp = new Date().getTime();
+                    return callback(null,doc);
                 }
-                // Cache invalid or does not exist
-                successCb(null);
             }
-        }); 
-    });
+            // Cache invalid or does not exist
+            return callback(null,null);
+        }
+    }); 
 }
 
-function setDoc(type, identifier,data) {
-	return new Promise(function(successCb, errorCb) {
-		data.cacheMeta = {
-			'cacheTimestamp' : new Date().getTime()
-		};
-        data.cacheKey = getCacheKey(type, identifier);
-        var dbdoc = {'Item' : data,
-                    'TableName' : cacheTable};
-	    dynamoDB.putItem(dbdoc, function(err, data) {
-	        if (err) {
-                return errorCb("Cache error: " + err); 
-            }
-            memcache[data.cacheKey] = data;
+function setDoc(type, identifier,data, callback) {
+    if (callback == null) {
+        callback = function() {};
+    }
+	data.cacheMeta = {
+		'cacheTimestamp' : new Date().getTime()
+	};
+    data.cacheKey = getCacheKey(type, identifier);
+    var dbdoc = {'Item' : data,
+                'TableName' : cacheTable};
+    dynamoDB.putItem(dbdoc, function(err, data) {
+        if (err) {
+            return errorCb("Cache error: " + err); 
+        }
+        memcache[data.cacheKey] = data;
 
-            return successCb(data);
-        });
+        return callback(null, data);
     });
 }
 
